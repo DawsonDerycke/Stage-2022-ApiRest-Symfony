7e95b80de9a76b735c3942e756becc51f1fcae6c
root <root@LPT-MEH.vynckier.local>
2022-03-23 09:16:50 +0100

    clone

--- public/index.php
+++ public/index.php
@@ -3,6 +3,14 @@
 // phpinfo();
 // exit();
 
+$serverName = "REPORTING"; //serverName
+$connectionInfo = array( "Database"=>"TEST_DAWSON2", "UID"=>"sa", "PWD"=>"vyn.ccs");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( !$conn ) {
+    echo "Connection could not be established.<br />";
+    die( print_r( sqlsrv_errors(), true));
+}
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
